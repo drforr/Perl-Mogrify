7595d82ef296cc88920566d0407a5c81fdcbf822
Jeffrey Goff <jgoff@cpan.org>
2015-08-07 21:57:40 +0200

    'my', 'our', 'print', other builtins need whitespace added too.

---
+++ lib/Perl/ToPerl6/Transformer/Builtins/AddWhitespace.pm
@@ -0,0 +1,103 @@
+package Perl::ToPerl6::Transformer::Builtins::AddWhitespace;
+
+use 5.006001;
+use strict;
+use warnings;
+use Readonly;
+
+use Perl::ToPerl6::Utils qw{ :characters :severities };
+use Perl::ToPerl6::Utils::PPI qw{ is_ppi_token_word };
+
+use base 'Perl::ToPerl6::Transformer';
+
+our $VERSION = '0.03';
+
+#-----------------------------------------------------------------------------
+
+Readonly::Scalar my $DESC => q{Transform qw(...) to qw (...)};
+Readonly::Scalar my $EXPL => q{Transform qw(...) to qw (...)};
+
+#-----------------------------------------------------------------------------
+
+my %map = (
+    my => 1,
+    our => 1,
+    print => 1,
+);
+
+sub supported_parameters { return ()                }
+sub default_severity     { return $SEVERITY_HIGHEST }
+sub default_themes       { return qw(core bugs)     }
+sub applies_to           {
+    return sub {
+        is_ppi_token_word($_[1], %map)
+    }
+}
+
+#-----------------------------------------------------------------------------
+
+sub transform {
+    my ($self, $elem, $doc) = @_;
+
+    if ( $elem->next_sibling and
+         $elem->next_sibling->isa('PPI::Token::Whitespace') ) {
+        $elem->insert_after(
+            PPI::Token::Whitespace->new(' ')
+        );
+    }
+
+    return $self->transformation( $DESC, $EXPL, $elem );
+}
+
+1;
+
+#-----------------------------------------------------------------------------
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Perl::ToPerl6::Transformer::Buiiltins::AddWhitespace - Format my(), our(), print()
+
+
+=head1 AFFILIATION
+
+This Transformer is part of the core L<Perl::ToPerl6|Perl::ToPerl6> distribution.
+
+
+=head1 DESCRIPTION
+
+Perl6 requires whitespace after C<my>, C<our>, C<print> etc. in order to not confuse these builtins with methods:
+
+  my() --> my ()
+  our() --> our ()
+  print() --> print ()
+
+=head1 CONFIGURATION
+
+This Transformer is not configurable except for the standard options.
+
+=head1 AUTHOR
+
+Jeffrey Goff <drforr@pobox.com>
+
+=head1 COPYRIGHT
+
+Copyright (c) 2015 Jeffrey Goff
+
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+##############################################################################
+# Local Variables:
+#   mode: cperl
+#   cperl-indent-level: 4
+#   fill-column: 78
+#   indent-tabs-mode: nil
+#   c-indentation-style: bsd
+# End:
+# ex: set ts=8 sts=4 sw=4 tw=78 ft=perl expandtab shiftround :
---
+++ t/Builtins/AddWhitespace.t
@@ -0,0 +1,20 @@
+#!perl
+
+use 5.006001;
+
+use strict;
+use warnings;
+
+use Test::Perl::ToPerl6::Transformer qw< transform_ok >;
+
+#-----------------------------------------------------------------------------
+
+our $VERSION = '0.01';
+
+transform_ok( 'Builtins::AddWhitespace', *DATA );
+
+__DATA__
+## name: transform
+my($a);
+##-->
+my ($a);
